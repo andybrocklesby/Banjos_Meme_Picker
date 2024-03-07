5ad99910836a41981c81bb96f4a4a352729922a2
AndyWilliam96 <153957322+andybrocklesby@users.noreply.github.com>
2024-03-07 14:40:28 -0600

    Initial push

---
+++ app.js
@@ -0,0 +1,96 @@
+import { boxerData } from './data.js'
+
+const locationToAppendRadio = document.getElementById('emotions-display')
+const getImageBtn = document.getElementById('get-image-btn')
+const isGifCheckbox = document.getElementById('is-gif-checkbox')
+const modal = document.getElementById('modal')
+const closeModalBtn = document.getElementById('modal-close-btn')
+const locationToAppendImage = document.getElementById('image-container')
+
+getImageBtn.addEventListener('click', renderImageInModal)
+closeModalBtn.addEventListener('click', function(){
+    modal.style.display = 'none';
+})
+
+function getEmotionsArray(boxers){
+
+    let emotionArray = []
+
+    for(let boxer of boxers){
+        for(let emotion of boxer.emotionTags){
+            if(!emotionArray.includes(emotion)){
+                emotionArray.push(emotion);
+
+            }
+        }
+    }
+
+    return emotionArray
+
+}
+
+function getSelectedEmotionsArray(){
+
+    const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
+    const isGif = isGifCheckbox.checked;
+
+    const selectedEmotionArray = boxerData.filter(function(boxer){
+        if(isGif){
+            return boxer.emotionTags.includes(selectedEmotion) && boxer.isGif
+        }
+        else{
+            return boxer.emotionTags.includes(selectedEmotion)
+        }
+    })
+    return selectedEmotionArray
+}
+
+function getSingleEmotionForRender(){
+    
+    const matchingArray = getSelectedEmotionsArray(boxerData);
+    let randomBoxer = Math.floor(Math.random() * matchingArray.length);
+
+    return matchingArray[randomBoxer];
+}
+
+function renderImageInModal(){
+
+    if(document.querySelector('input[type="radio"]:checked')){
+        const boxer = getSingleEmotionForRender(boxerData)
+
+        modal.style.display = 'block'
+
+        locationToAppendImage.innerHTML = `
+            <img
+                class="rendered-image"
+                src="./images/${boxer.image}"
+                alt="${boxer.alt}">
+        `
+    }
+
+}
+
+function renderEmotionRadios(){
+
+    const emotionArray = getEmotionsArray(boxerData);
+    let newEmotionRadio = ''
+
+    for(let emotion of emotionArray){
+        newEmotionRadio += `
+            <div class="radio flex-row">
+            <label class="emotion-label" for="${emotion}">${emotion}</label>
+            <input 
+                class="emotion-radio"
+                type="radio"
+                id="${emotion}"
+                value="${emotion}"
+                name="emotion">
+            </div> 
+        `
+    }
+
+    locationToAppendRadio.innerHTML = newEmotionRadio
+
+}
+
+renderEmotionRadios()
---
+++ data.js
@@ -0,0 +1,122 @@
+export const boxerData = [
+    {
+        emotionTags: ['confused'],
+        isGif: true,
+        image: 'confused-boxer-1.webp',
+        alt: 'A boxer looking confused'
+    },
+    {
+        emotionTags: ['confused'],
+        isGif: false,
+        image: 'confused-boxer-2.jpeg',
+        alt: 'A boxer looking confused'
+    },
+    {
+        emotionTags: ['cool', 'no thoughts'],
+        isGif: true,
+        image: 'cool-boxer-1.webp',
+        alt: 'A boxer looking cool'
+    },
+    {
+        emotionTags: ['cool', 'no thoughts'],
+        isGif: false,
+        image: 'cool-boxer-2.jpeg',
+        alt: 'A boxer looking cool'
+    },
+    {
+        emotionTags: ['curious', 'funny'],
+        isGif: true,
+        image: 'curious-boxer-1.webp',
+        alt: 'A boxer looking curious'
+    },
+    {
+        emotionTags: ['curious'],
+        isGif: false,
+        image: 'curious-boxer-2.jpeg',
+        alt: 'A boxer looking curious'
+    },
+    {
+        emotionTags: ['excited', 'funny', 'happy'],
+        isGif: true,
+        image: 'excited-boxer-1.webp',
+        alt: 'A boxer looking excited'
+    },
+    {
+        emotionTags: ['excited', 'funny', 'happy'],
+        isGif: false,
+        image: 'excited-boxer-2.jpeg',
+        alt: 'A boxer looking excited'
+    },
+    {
+        emotionTags: ['funny'],
+        isGif: true,
+        image: 'funny-boxer-1.webp',
+        alt: 'A boxer looking funny'
+    },
+    {
+        emotionTags: ['funny'],
+        isGif: false,
+        image: 'funny-boxer-2.avif',
+        alt: 'A boxer looking funny'
+    },
+    {
+        emotionTags: ['happy'],
+        isGif: true,
+        image: 'happy-boxer-1.webp',
+        alt: 'A boxer looking happy'
+    },
+    {
+        emotionTags: ['happy'],
+        isGif: false,
+        image: 'happy-boxer-2.webp',
+        alt: 'A boxer looking happy'
+    },
+    {
+        emotionTags: ['hungry'],
+        isGif: true,
+        image: 'hungry-boxer-1.webp',
+        alt: 'A boxer looking hungry'
+    },
+    {
+        emotionTags: ['hungry', 'funny'],
+        isGif: false,
+        image: 'hungry-boxer-2.jpeg',
+        alt: 'A boxer looking hungry'
+    },
+    {
+        emotionTags: ['lazy', 'funny'],
+        isGif: true,
+        image: 'lazy-boxer-1.webp',
+        alt: 'A boxer looking lazy'
+    },
+    {
+        emotionTags: ['lazy', 'funny'],
+        isGif: false,
+        image: 'lazy-boxer-2.jpeg',
+        alt: 'A boxer looking lazy'
+    },
+    {
+        emotionTags: ['loving'],
+        isGif: true,
+        image: 'loving-boxer-1.webp',
+        alt: 'A boxer looking loving'
+    },
+    {
+        emotionTags: ['loving'],
+        isGif: false,
+        image: 'loving-boxer-2.jpeg',
+        alt: 'A boxer looking loving'
+    },
+    {
+        emotionTags: ['no thoughts', 'funny'],
+        isGif: true,
+        image: 'nothoughts-boxer-1.webp',
+        alt: 'A boxer looking no thoughts'
+    },
+    {
+        emotionTags: ['no thoughts'],
+        isGif: false,
+        image: 'nothoughts-boxer-2.jpeg',
+        alt: 'A boxer looking no thoughts'
+    },
+]
